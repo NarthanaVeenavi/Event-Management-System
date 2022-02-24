858ab54ee8d5c3cbbe809b7586c794c304ef7374
unknown <narthanamendis1997@gmail.com>
2022-02-24 11:53:04 +0530

    Completed project

--- BACKEND/controllers/eventCtrl.js
+++ BACKEND/controllers/eventCtrl.js
@@ -1,45 +1,45 @@
 const Events = require('../Models/eventModel')
 
-// // Filter, sorting and paginating
-// class APIfeatures {
-//     constructor(query, queryString){
-//         this.query = query;
-//         this.queryString = queryString;
-//     }
-//     filtering(){
-//        const queryObj = {...this.queryString} //queryString = req.query
+// Filter, sorting and paginating
+class APIfeatures {
+    constructor(query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    }
+    filtering(){
+       const queryObj = {...this.queryString} //queryString = req.query
 
-//        const excludedFields = ['page', 'sort', 'limit']
-//        excludedFields.forEach(el => delete(queryObj[el]))
+       const excludedFields = ['page', 'sort', 'limit']
+       excludedFields.forEach(el => delete(queryObj[el]))
        
-//        let queryStr = JSON.stringify(queryObj)
-//        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+       let queryStr = JSON.stringify(queryObj)
+       queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
 
   
-//        this.query.find(JSON.parse(queryStr))
+       this.query.find(JSON.parse(queryStr))
          
-//        return this;
-//     }
+       return this;
+    }
 
-//     sorting(){
-//         if(this.queryString.sort){
-//             const sortBy = this.queryString.sort.split(',').join(' ')
-//             this.query = this.query.sort(sortBy)
-//         }else{
-//             this.query = this.query.sort('-createdAt')
-//         }
+    sorting(){
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        }else{
+            this.query = this.query.sort('-createdAt')
+        }
 
-//         return this;
-//     }
+        return this;
+    }
 
-//     paginating(){
-//         const page = this.queryString.page * 1 || 1
-//         const limit = this.queryString.limit * 1 || 9
-//         const skip = (page - 1) * limit;
-//         this.query = this.query.skip(skip).limit(limit)
-//         return this;
-//     }
-// }
+    paginating(){
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
+    }
+}
 
 const eventCtrl = {
 
